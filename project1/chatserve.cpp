9da7fdea839d6d1cc329fd832cd1559b52f67961
David Rigert <rigertd@onid.orst.edu>
2016-05-01 11:06:48 -0700

    Updated chatserve to be multithreaded with nonblocking input.

--- project1/chatserve.cpp
+++ project1/chatserve.cpp
@@ -1,31 +1,113 @@
 #include <iostream>
+#include <mutex>
 #include <stdexcept>
+#include <string>
+#include <thread>
 #include <unistd.h>
+#include <vector>
 
+#include "SafeQueue.hpp"
 #include "Socket.hpp"
 #include "SocketStream.hpp"
+
+SafeQueue<std::string> outgoing;
+
+std::vector<SocketStream> clients;
+std::mutex clients_mutex;
+
+
+void get_input() {
+    std::string buf;
+    while (true) {
+        std::getline(std::cin, buf);
+        if (!buf.empty()) {
+            outgoing.enqueue(buf);
+        }
+    }
+}
+
+void handle_clients() {
+    while (true) {
+        std::string out_message;
+        std::string in_message;
+        
+        // Get one outgoing server message (if any)
+        if (!outgoing.empty()) {
+            out_message = outgoing.dequeue();
+        }
+        
+        // Handle sending and receiving of messages from clients
+        std::lock_guard<std::mutex> guard(clients_mutex);
+        auto it = clients.begin();
+        while (it != clients.end()) {
+            // Send the server message (if any)
+            if (!out_message.empty())
+                it->send(message);
+            // Receive any messages from client
+            if (it->recv(message)) {
+                if (!message.empty()) {
+                    // Message received -- display to console
+                    std::cout << message << std::endl;
+                    // and send to each connected client
+                    auto it2 = clients.begin();
+                    while (it2 != clients.end()) {
+                        if (it != it2)
+                            it2->send(message);
+                        ++it2;
+                    }
+                }
+                
+                ++it;
+            }
+            else {
+                // Socket closed -- remove client
+                it = clients.erase(it);
+            }
+        }
+        
+        // Sleep for 1 millisecond to avoid eating up too much CPU time
+        ::usleep(1000);
+    }
+}
 
 int main(int argc, char* argv[]) {
 
     Socket s = Socket();
+    ::fd_set readable;
+    int fdmax;
+    std::vector<std::thread> clients;
 
+    // Start listening for connections
     try {
         s.listen("51423");
-        SocketStream ss = s.accept();
-        std::cout << "Received connection from: "
-            << s.get_dest_host() << ":" 
-            << s.get_dest_port() << std::endl;
-
-        std::string message;
-        while (ss.recv(message)) {
-            if (!message.empty())
-                std::cout << message << std::endl;
-            std::getline(std::cin, message);
-            ss.send(message);
-        }
+        std::cout << "Waiting for connections..." << std::endl;
     }
     catch (const std::runtime_error& ex) {
         std::cout << ex.what() << std::endl;
+    }
+
+    // Start input thread
+    std::thread input_thread (get_input);
+    
+    // Start client handling thread
+    std::thread client_thread (handle_clients);
+
+    // Accept incoming connections until interrupt
+    while (true) {
+        try {
+            SocketStream ss = s.accept();
+            std::cout << "Received connection from: "
+                << s.get_dest_host() << ":" 
+                << s.get_dest_port() << std::endl;
+
+            // Add new socket to list of currently connected clients
+            std::lock_guard<std::mutex> guard(clients_mutex);
+            clients.push_back(ss);
+        }
+        catch (const std::runtime_error& ex) {
+            std::cout << ex.what() << std::endl;
+        }
+
     }
 
     return 0;

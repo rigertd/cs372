02482fa6b4d5e04e84d7be56d1afab2009842de4
David Rigert <rigertd@onid.orst.edu>
2016-05-22 15:47:12 -0700

    Merged Socket class into ftclient file.

--- project2/ftclient.py
+++ project2/ftclient.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python
 """
-chatclient: A simple TCP-based chat client program.
+chatclient: A simple TCP-based file transfer client program.
 
 Author:     David Rigert
 Class:      CS372 Spring 2016
-Assignment: Project #1
+Assignment: Project #2
 
-When this program is run, the user is prompted to enter their handle.
-The handle will prepend any messages sent from this client. Once the user
-enters a message to send, this program attempts to connect to the chatserv
-program. After a connection is established, any messages sent from this program
-are displayed on the server and any other connected clients, and any messages
-sent by the server and other connected clients are displayed in this program.
+This program connects to ftserve and either requests a directory listing
+or the transfer of a file. All files are transferred as binary data.
 
-    Command-line syntax: ./chatclient <server_hostname> <server_port>
+    Command-line syntax:
+        ./ftclient.py server_host server_port (-l | -g FILENAME) data_port
 
 This program takes the following arguments:
-    - server_hostname       -- The hostname or IP address of the server
-    - server_port           -- The port number that the server is listening on
+    - server_host   -- Hostname or IP address of the server running ftserve
+    - server_port   -- Port number that the server is listening on
+    - -l, --list    -- Tells the server to send a list of files
+    - -g, --get     -- Tells the server to send the specified FILENAME
+    - data_port     -- Port number over which server sends data to client
 """
 
 __author__      = "David Rigert"
 
-import readline
 import sys
-import select
-import time
 import re
 import os
-from Socket import Socket
+import socket
 from argparse import ArgumentParser
 
 
@@ -109,15 +106,22 @@ def main():
 
 def get_unique_filename(name):
     """
-    Returns a filename that is guaranteed to be unique in the specified directory.
+    Returns a filename that is unique in the specified directory.
+
+    This function appends a number to the end of the filename and before
+    the file extension. The number is incremented until a unique filename
+    is found.
     """
     if not os.path.exists(name):
         return name
     else:
+        # Match filename followed by number in parens and then optional extension
         m = re.match(r'(.*\()(\d+)(\))(\..+)?', name, re.IGNORECASE)
         if m:
+            # Use a list comprehension to skip extension if None
             name = '{0}{1}{2}'.format(m.groups()[0], int(m.groups()[1]) + 1, ''.join(g for g in m.groups()[2:] if g is not None))
         else:
+            # Does not have parens w/ number: just add one before the extension
             parts = name.split('.')
             if len(parts) > 1:
                 name = '.'.join(parts[:-1]) + '(1).' + parts[-1]
@@ -158,6 +162,136 @@ def is_int(val):
         return True
     except ValueError:
         return False
+
+class Socket:
+    """
+    Creates a TCP socket using the socket library.
+
+    This class provides a number of methods to interact with the socket.
+    The send and receive functions return a bool value indicating whether
+    the socket is still open.
+    """
+
+    def __init__(self, sock=None):
+        """
+        Initializes the object as a TCP socket.
+        """
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        """
+        Attempts to connect to the specified host and port.
+        """
+        self.sock.connect((host, int(port)))
+
+    def close(self):
+        """
+        Explicitly closes the socket.
+        """
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+
+    def listen(self, port):
+        """
+        Listens for incoming connections on the specified port.
+
+        Based on code in https://docs.python.org/3/library/socket.html
+        """
+        #Attempt to bind on the specified port using IPv4 or IPv6
+        for family, socktype, protocol, canonname, sockaddr in socket.getaddrinfo(None, port, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+            # Attempt to open a socket based on localhost address info
+            try:
+                self.sock = socket.socket(family, socktype, protocol)
+            except OSError as ex:
+                self.sock = None
+                continue
+
+            # Configure the socket to reuse ports
+            try:
+                self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            except OSError as ex:
+                self.sock = None
+                continue
+
+            # Attempt to bind the sockaddr and listen
+            try:
+                self.sock.bind(sockaddr)
+            except OSError as ex:
+                self.sock.close()
+                s = None
+                continue
+            # successfully bound if we reach here
+            break
+
+        if self.sock is None:
+            raise Exception('bind: No valid address found')
+
+        # Try to listen on the socket
+        try:
+            self.sock.listen(1) # only accept 1 connection
+        except OSError as ex:
+            raise Exception('listen: ' + ex)
+
+    def accept(self):
+        """
+        Accepts an incoming connection.
+
+        Returns a Socket object for the new connection.
+        """
+        newSock, self.remote_address = self.sock.accept()
+        return Socket(newSock)
+
+    def send(self, data):
+        """
+        Sends all of the data unless the socket is closed before it finishes.
+        Returns whether the socket is still open.
+        """
+        bytes = 0
+        while (len(data) > 0):
+            bytes = self.sock.send(data)
+            if (bytes == 0):
+                # Socket was closed, return false
+                return False
+
+            data = data[bytes:]
+
+        # Return true if socket is still open
+        return True
+
+    def recv(self):
+        """
+        Receives up to 500 bytes of data.
+        Returns a tuple including whether the socket is still open,
+        and the received data (or an empty string if none).
+        """
+        buf = self.sock.recv(500)
+
+        if len(buf) == 0:
+            return False, buf
+        else:
+            return True, buf
+
+    def recv_all(self, length):
+        """
+        Receives the specified number of bytes of data.
+        Returns a tuple including whether the socket is still open,
+        and the received data (or an empty string if none).
+        """
+        received = 0
+        buf = []
+        while received < length:
+            data = self.sock.recv(500)
+            # Return False and anything that was received if the socket was closed
+            if len(data) == 0:
+                return False, ''.join(buf)
+            # Otherwise append to data already received
+            received += len(data)
+            buf.append(data)
+
+        return True, ''.join(buf)
 
 # This just makes sure the code doesn't run when imported as a module
 if __name__ == '__main__':
